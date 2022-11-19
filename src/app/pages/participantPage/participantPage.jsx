ab5f67b326a5fc4631c461c40dff8e1b2dab3f0b
DmitriyZabava <102691652+DmitriyZabava@users.noreply.github.com>
2022-11-19 15:05:52 +0300

    change participat about

--- src/app/pages/participantPage/participantPage.jsx
+++ src/app/pages/participantPage/participantPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import AboutParticipant from "../../components/ui/aboutParticipant";
 import API from "../../api";
-import Button from "../../components/button";
 
 const ParticipantPage = () => {
     const { userId } = useParams();
@@ -8,8 +9,37 @@ const ParticipantPage = () => {
     useEffect(() => {
         API.users.getById(userId).then((data) => setParticipant(data));
     }, []);
-    useEffect(() => console.log(participants), [participants]);
-    return <h1>Participant page</h1>;
+    useEffect(() => {
+        console.log("participant", participant);
+    }, [participant]);
+    if (!participant) return "...Loader";
+    return (
+        <div className="participant">
+            <div className="container participant__container">
+                <div className="participant__about">
+                    <AboutParticipant {...participant} />
+
+                    <div className="participant__links">
+                        <Link
+                            className="participant__link"
+                            href="#"
+                            target="_blank"
+                        >
+                            <i className="bx bxl-vk"></i>
+                        </Link>
+                    </div>
+                </div>
+
+                <div className="participant__box">
+                    <img
+                        className="participant__box-img"
+                        src={participant.photo}
+                        alt="Avatar"
+                    />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default ParticipantPage;
