be11a85beb78f7eca0fb667e0fdcc0956ef27e17
Anton Lebedev <sorrint@gmail.com>
2022-11-19 20:49:09 +0800

    fix lint errors

--- src/app/pages/participantPage/participantPage.jsx
+++ src/app/pages/participantPage/participantPage.jsx
@@ -6,7 +6,6 @@ import API from "../../api";
 import BreadCrumbs from "../../components/breadCrumbs";
 import Navbar from "../../components/navBar";
 
-
 const ParticipantPage = () => {
     const { userId } = useParams();
     const location = useLocation();
@@ -19,34 +18,35 @@ const ParticipantPage = () => {
         console.log("participant", participant);
     }, [participant]);
     if (!participant) return "...Loader";
-    return (<>
-     <Navbar />
-                <BreadCrumbs pathname={location.pathname} />
-        <div className="participant">
-            <div className="container participant__container">
-                <div className="participant__about">
-                    <AboutParticipant {...participant} />
+    return (
+        <>
+            <Navbar />
+            <BreadCrumbs pathname={location.pathname} />
+            <div className="participant">
+                <div className="container participant__container">
+                    <div className="participant__about">
+                        <AboutParticipant {...participant} />
 
-                    <div className="participant__links">
-                        <Link
-                            className="participant__link"
-                            href="#"
-                            target="_blank"
-                        >
-                            <i className="bx bxl-vk"></i>
-                        </Link>
+                        <div className="participant__links">
+                            <Link
+                                className="participant__link"
+                                href="#"
+                                target="_blank"
+                            >
+                                <i className="bx bxl-vk"></i>
+                            </Link>
+                        </div>
+                    </div>
+
+                    <div className="participant__box">
+                        <img
+                            className="participant__box-img"
+                            src={participant.photo}
+                            alt="Avatar"
+                        />
                     </div>
                 </div>
-
-                <div className="participant__box">
-                    <img
-                        className="participant__box-img"
-                        src={participant.photo}
-                        alt="Avatar"
-                    />
-                </div>
             </div>
-        </div>
         </>
     );
 };
