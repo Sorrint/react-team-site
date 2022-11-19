29ef0d24bd437b4d3c412cd277fd85e1ae76b893
Anton Lebedev <sorrint@gmail.com>
2022-11-19 22:53:08 +0800

    add progress bar

--- src/app/pages/favouritePage/favouritePage.jsx
+++ src/app/pages/favouritePage/favouritePage.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 import BreadCrubms from "../../components/breadCrumbs";
 import Navbar from "../../components/navBar";
+import Progress from "../../components/progress";
 
 const FavouritePage = () => {
     const location = useLocation();
     return (
         <>
-            <BreadCrubms pathname={location.pathname} />
             <Navbar />
-            <h1>Favourite Page</h1>
+            <BreadCrubms pathname={location.pathname} />
+            <div className="wrapper">
+                <Progress value="30" name="CSS" type="circle" />
+            </div>
         </>
     );
 };
