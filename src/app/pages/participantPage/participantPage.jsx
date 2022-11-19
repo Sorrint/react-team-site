0d6ddf3c8fbdbd12e0b2d16ba588df4649235fc7
Shabalin_Yakov <shab.yakov@ya.ru>
2022-11-19 15:09:56 +0500

    added component Button

--- src/app/pages/participantPage/participantPage.jsx
+++ src/app/pages/participantPage/participantPage.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
-
 import API from "../../api";
-import Button from "../../components/button";
 
 const ParticipantPage = () => {
     const [participants, setParticipants] = useState();
@@ -9,12 +7,7 @@ const ParticipantPage = () => {
         API.users.fetchAll().then((data) => setParticipants(data));
     }, []);
     useEffect(() => console.log(participants), [participants]);
-    return (
-        <>
-            <h1>Participant page</h1>
-            <Button color="blue" border="rounded" name="app" content="Name" />
-        </>
-    );
+    return <h1>Participant page</h1>;
 };
 
 export default ParticipantPage;
