fcd48ce41e98691f7cfc0e6ce743c1af59d18180
Eugene015 <egavriluk@gmail.com>
2022-11-20 13:11:20 +0100

    contacts

--- src/app/components/common/conacts.jsx
+++ src/app/components/common/conacts.jsx
@@ -1,8 +1,23 @@
-const Contacts = (contacts) => {
+import React from "react";
+import PropTypes from "prop-types";
+
+const Contacts = ({ contacts }) => {
     console.log(contacts);
-    if (contacts) {
-        return `контакты`;
-    }
+    return (
+        <ul className="text-[#8892b0] flex py-10">
+            {contacts.map((cont) => {
+                return (
+                    <li key={cont.name}>
+                        <a href={cont.path}>{cont.name}</a>
+                    </li>
+                );
+            })}
+        </ul>
+    );
+};
+
+Contacts.propTypes = {
+    contacts: PropTypes.array.isRequired
 };
 
 export default Contacts;
